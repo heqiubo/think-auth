a47ca607f7d4e38f85497bf52fa96c5d02314183
秋波 <344055687@qq.com>
2018-09-27 09:56:34 +0800

    支持tp5.1

--- src/Auth.php
+++ src/Auth.php
@@ -1,18 +1,10 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP 5 [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2016 http://www.zzstudio.net All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: Byron Sampson <xiaobo.sun@gzzstudio.net>
-// +----------------------------------------------------------------------
-namespace think;
+
+namespace think\auth;
 
 use think\Db;
-use think\Config;
-use think\Session;
+use think\facade\Config;
+use think\facade\Session;
 use think\Request;
 use think\Loader;
 
@@ -21,57 +13,16 @@ use think\Loader;
  * 功能特性：
  * 1，是对规则进行认证，不是对节点进行认证。用户可以把节点当作规则名称实现对节点进行认证。
  *      $auth=new Auth();  $auth->check('规则名称','用户id')
+ *
  * 2，可以同时对多条规则进行认证，并设置多条规则的关系（or或者and）
  *      $auth=new Auth();  $auth->check('规则1,规则2','用户id','and')
  *      第三个参数为and时表示，用户需要同时具有规则1和规则2的权限。 当第三个参数为or时，表示用户值需要具备其中一个条件即可。默认为or
+ *
  * 3，一个用户可以属于多个用户组(think_auth_group_access表 定义了用户所属用户组)。我们需要设置每个用户组拥有哪些规则(think_auth_group 定义了用户组权限)
  *
  * 4，支持规则表达式。
  *      在think_auth_rule 表中定义一条规则时，如果type为1， condition字段就可以定义规则表达式。 如定义{score}>5  and {score}<100  表示用户的分数在5-100之间时这条规则才会通过。
  */
-//数据库
-/*
--- ----------------------------
--- think_auth_rule，规则表，
--- id:主键，name：规则唯一标识, title：规则中文名称 status 状态：为1正常，为0禁用，condition：规则表达式，为空表示存在就验证，不为空表示按照条件验证
--- ----------------------------
-DROP TABLE IF EXISTS `think_auth_rule`;
-CREATE TABLE `think_auth_rule` (
-    `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-    `name` char(80) NOT NULL DEFAULT '',
-    `title` char(20) NOT NULL DEFAULT '',
-    `type` tinyint(1) NOT NULL DEFAULT '1',
-    `status` tinyint(1) NOT NULL DEFAULT '1',
-    `condition` char(100) NOT NULL DEFAULT '',  # 规则附件条件,满足附加条件的规则,才认为是有效的规则
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
--- ----------------------------
--- think_auth_group 用户组表，
--- id：主键， title:用户组中文名称， rules：用户组拥有的规则id， 多个规则","隔开，status 状态：为1正常，为0禁用
--- ----------------------------
-DROP TABLE IF EXISTS `think_auth_group`;
-    CREATE TABLE `think_auth_group` (
-    `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-    `title` char(100) NOT NULL DEFAULT '',
-    `status` tinyint(1) NOT NULL DEFAULT '1',
-    `rules` char(80) NOT NULL DEFAULT '',
-    PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
--- ----------------------------
--- think_auth_group_access 用户组明细表
--- uid:用户id，group_id：用户组id
--- ----------------------------
-DROP TABLE IF EXISTS `think_auth_group_access`;
-    CREATE TABLE `think_auth_group_access` (
-    `uid` mediumint(8) unsigned NOT NULL,
-    `group_id` mediumint(8) unsigned NOT NULL,
-    UNIQUE KEY `uid_group_id` (`uid`,`group_id`),
-    KEY `uid` (`uid`),
-    KEY `group_id` (`group_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
- */
-
 class Auth
 {
     /**
@@ -91,7 +42,7 @@ class Auth
         'auth_group' => 'auth_group', // 用户组数据表名
         'auth_group_access' => 'auth_group_access', // 用户-用户组关系表
         'auth_rule' => 'auth_rule', // 权限规则表
-        'auth_user' => 'member', // 用户信息表
+        'auth_user' => 'user', // 用户信息表
     ];
 
     /**
@@ -100,12 +51,7 @@ class Auth
      */
     public function __construct()
     {
-        //可设置配置项 auth, 此配置项为数组。
-        if ($auth = Config::get('auth')) {
-            $this->config = array_merge($this->config, $auth);
-        }
-        // 初始化request
-        $this->request = Request::instance();
+        $this->request = new Request();
     }
 
     /**
@@ -231,12 +177,11 @@ class Auth
             return [];
         }
         $map = array(
-            'id' => ['in', $ids],
             'type' => $type,
             'status' => 1,
         );
         //读取用户组所有权限规则
-        $rules = Db::name($this->config['auth_rule'])->where($map)->field('condition,name')->select();
+        $rules = Db::name($this->config['auth_rule'])->whereIn('id', $ids)->where($map)->field('condition,name')->select();
         //循环规则，判断结果。
         $authList = []; //
         foreach ($rules as $rule) {
@@ -261,33 +206,6 @@ class Auth
         }
 
         return array_unique($authList);
-    }
-
-    /**
-     * 获取权限组对应的权限列表
-     * @param $gid
-     * @param int $type
-     * @return array|mixed
-     */
-    public function getGroupAuthList($gid, $type = 1)
-    {
-        // 转换表名
-        $auth_group = $this->config['auth_group'];
-        $auth_rule = $this->config['auth_rule'];
-        // 执行查询
-        $rules = Db::name($auth_group)->where(['status'=>1, 'id'=>$gid])->value('rules');
-        // 格式化access表id
-        $ids = explode(',', trim($rules, ','));
-        $ids = array_unique($ids);
-        $map = array(
-            'id' => ['in', $ids],
-            'type' => $type,
-            'status' => 1,
-        );
-        //读取用户组所有权限规则
-        $rules = Db::name($auth_rule)->where($map)->column('title,name,condition');
-
-        return $rules;
     }
 
     /**
